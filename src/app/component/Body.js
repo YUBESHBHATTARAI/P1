d1817821616698e295f0280e99784fa4ee8a52bd
Yubesh <yubeshbhattarai@gmail.com>
2025-06-12 09:42:37 +0545

    commit

--- src/app/component/Body.js
+++ src/app/component/Body.js
@@ -6,18 +6,17 @@ function Body(){
 
     return(
         <>
-        <div>
+        <div className="flex justify-center items-center">
         {/* left */}
         <div>
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis culpa exercitationem harum dolor dolorem odio ipsam corrupti? Eum facere officia sequi voluptatum explicabo. Voluptatum doloribus nobis illo id temporibus?
-
+        <div className="text-2xl font-cursive overflow-auto">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi praesentium dolorum pariatur eveniet distinctio laboriosam delectus quisquam, vel eius maiores explicabo odit ab sapiente iure, et omnis rem earum fuga alias! Cupiditate at minus labore dicta alias repellendus deserunt temporibus ipsum id facere. Tenetur ut animi maiores dolorem. Dolore nam similique repudiandae illum ex quas excepturi? Nostrum itaque eos inventore consequuntur deserunt excepturi sit odio illum, delectus, totam vitae commodi perferendis, necessitatibus deleniti dolorem tempore aut officia eum. Labore vel commodi enim odio repudiandae tenetur libero deleniti illo perferendis nam architecto nesciunt eveniet accusamus doloribus, beatae, adipisci esse molestias. Assumenda.
         </div>
         </div>
         {/* right */}
         <div>
-        <div>
-         <img src="f1/public/all.png"/>
+        <div className="shadow-2xl m-2 p-5">
+         <img className="h-[400px] w-[600px]  rounded-xl "  src="/all.png"/>
         </div>
         </div>
 
